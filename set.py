4359bea9d04ebd3a9eefb546aed57de782c5ac7c
83CarloG <cgcolombo1983@gmail.com>
2023-05-22 15:26:58 +0200

    add exercises

---
+++ set.py
@@ -0,0 +1,49 @@
+# Esercizio 1
+# Creare un set vuoto e assegnarlo a una variabile.
+# Esercizio 2
+# Creare un set contenente i seguenti elementi: "mela", "banana", "kiwi", "mela".
+# Esercizio 3
+# Aggiungere l'elemento "pesca" al set precedente.
+# Esercizio 4
+# Rimuovere l'elemento "mela" dal set precedente.
+# Esercizio 5
+# Verificare se l'elemento "ananas" è presente nel set precedente.
+# Esercizio 6
+# Creare un nuovo set contenente gli elementi "banana", "kiwi" e "arancia".
+# Esercizio 7
+# Creare un set contenente i numeri interi da 1 a 5 ricavati da un range().
+# Esercizio 8 (difficile)
+# Creare un nuovo set contenente i numeri pari del set precedente.
+
+#1
+set_example = set(())
+print(set_example)
+#2
+frutti =  {"mela", "banana", "kiwi", "mela"}
+print(frutti,type(frutti))
+#3
+frutti.add("pesca")
+print(frutti)
+#4
+frutti.remove("mela")
+print(frutti)
+#5
+if "anans" in frutti:
+    print("ok")
+else:
+    print("ko")
+#6
+nuovo_set = {"banana", "kiwi", "arancia"}
+print(nuovo_set)
+#
+nuovo_set_range = set((range(1,6)))
+print(nuovo_set_range)
+#
+nuovo_set_range_pari = set(())
+for numero in nuovo_set_range:
+    if numero%2:
+        nuovo_set_range_pari.add(numero)
+
+#numeri_pari = set(x for x in numeri if x % 2 == 0)
+
+print(nuovo_set_range_pari)
---
+++ tuple.py
@@ -0,0 +1,52 @@
+# Esercizio 1
+# Creare una tupla vuota e assegnarla a una variabile.
+# Esercizio 2
+# Creare una tupla con i seguenti elementi: "mela", "banana", "kiwi".
+# Esercizio 3
+# Accedere all'elemento "banana" della tupla precedente.
+# Esercizio 4
+# Creare una nuova tupla che contenga solo i primi due elementi della tupla precedente.
+# Esercizio 5
+# Verificare se l'elemento "ananas" è presente nella tupla precedente.
+# Esercizio 6
+# Creare una nuova tupla concatenando la tupla precedente con la tupla ("pesca", "arancia").
+# Esercizio 7
+# Verificare la lunghezza della tupla precedente.
+# Esercizio 8
+# Creare una tupla contenente i numeri interi da 1 a 5.
+# Esercizio 9 (difficile)
+# Creare una tupla contenente il quadrato dei numeri interi da 1 a 5.
+# Esercizio 10
+# Contare il numero di occorrenze dell'elemento "mela" nella tupla precedente.
+
+#1
+tuple_example = ()
+# tuple_example = tuple(())
+print(type(tuple_example))
+#2
+tuple_example2 = ("mela", "banana", "kiwi")
+print(tuple_example2)
+#3
+print(tuple_example2[1])
+#4
+tuple_example3 = tuple_example2[0:2]
+print(tuple_example3)
+#5
+if "anans" in tuple_example3:
+    print("ok")
+else:
+    print("ko")
+#6
+tuple_example5 = tuple_example2 + ("pesca" , "albicocca")
+print(tuple_example5)
+#7
+print(len(tuple_example5))
+#8
+numeri = (1,2,3,4,5)
+print(numeri)
+9
+#9
+numeri_quadrato = tuple(numero**2 for numero in numeri)
+print(numeri_quadrato)
+#10
+print(tuple_example5.count("mela"))
